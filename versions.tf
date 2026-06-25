7e2d0ad1ce897e34eef1f495c7e9f58f8c9c2740
ronak0903 <ronaksolanki@bitcot.com>
2026-06-25 11:38:30 +0530

    opt for workspace

---
+++ versions.tf
@@ -0,0 +1,17 @@
+terraform {
+
+  cloud {
+
+    organization = "ronakHCP"
+
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
